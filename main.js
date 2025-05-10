101a450986649db7a8f6e55d6551eaa2a9cf567e
Jay Myers <jcmyers@protonmail.com>
2025-05-10 11:03:40 -0400

    Fix: start with BusinessSetupScene to enable setup form

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import SceneManager from "./scenes/SceneManager.js";
-import MainMenu from "./scenes/MainMenu.js";
+import BusinessSetupScene from "./scenes/BusinessSetupScene.js";
+import GameScene from "./scenes/GameScene.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -8,8 +9,9 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 window.sceneManager = new SceneManager(ctx);
-window.sceneManager.addScene("MainMenu", new MainMenu());
-window.sceneManager.start("MainMenu");
+window.sceneManager.addScene("BusinessSetupScene", new BusinessSetupScene());
+window.sceneManager.addScene("GameScene", new GameScene());
+window.sceneManager.start("BusinessSetupScene");
 
 function gameLoop() {
   window.sceneManager.update();
